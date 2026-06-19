fd3fc212330c8b144d21fa7bde21d94ea6016850
adeeliqbalanjum <bitm-f20-090@superior.edu.pk>
2026-06-19 20:42:40 +0500

    Load polished orbit styles

--- app/process-options/layout.tsx
+++ app/process-options/layout.tsx
@@ -1,6 +1,7 @@
 import "../process-extra-options.css";
 import "../process-gallery-fix.css";
 import "../process-real-options.css";
+import "../process-orbit-polish.css";
 
 export default function ProcessOptionsLayout({ children }: { children: React.ReactNode }) {
   return children;
