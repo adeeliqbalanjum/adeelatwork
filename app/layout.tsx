d48ed2c5e89ff3f3831c30886c099dedcae737f0
adeeliqbalanjum <bitm-f20-090@superior.edu.pk>
2026-06-19 18:42:24 +0500

    Import single scrollbar fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import './stats-motion.css';
 import './testimonials.css';
 import './testimonials-mount.css';
+import './scrollbar-fix.css';
 import { ButtonFillController } from './components/ButtonFillController';
 import { HomeTestimonialsMount } from './components/HomeTestimonialsMount';
 import { ScrollProgressBar } from './components/ScrollProgressBar';
