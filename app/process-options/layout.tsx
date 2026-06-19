2c2d19e446cd282d321a6c0ed303e680019e32c1
adeeliqbalanjum <bitm-f20-090@superior.edu.pk>
2026-06-19 20:03:51 +0500

    Load sticky gallery animation fix

--- app/process-options/layout.tsx
+++ app/process-options/layout.tsx
@@ -1,4 +1,5 @@
 import "../process-extra-options.css";
+import "../process-gallery-fix.css";
 
 export default function ProcessOptionsLayout({ children }: { children: React.ReactNode }) {
   return children;
