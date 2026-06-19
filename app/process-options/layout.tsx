e6128c36aec1a260f3ee28a8ca3d422fe2ceced1
adeeliqbalanjum <bitm-f20-090@superior.edu.pk>
2026-06-19 20:30:59 +0500

    Load real process motion styles

--- app/process-options/layout.tsx
+++ app/process-options/layout.tsx
@@ -1,5 +1,6 @@
 import "../process-extra-options.css";
 import "../process-gallery-fix.css";
+import "../process-real-options.css";
 
 export default function ProcessOptionsLayout({ children }: { children: React.ReactNode }) {
   return children;
