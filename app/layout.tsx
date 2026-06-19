d256fc28bc431c22b8c53793374e3fee9c552384
adeeliqbalanjum <bitm-f20-090@superior.edu.pk>
2026-06-19 23:32:34 +0500

    Load build stack card polish styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import './home-process-tweaks.css';
 import './build-stack.css';
 import './build-stack-fix.css';
 import './build-stack-icons.css';
+import './build-stack-card-polish.css';
 import './scrollbar-fix.css';
 import { ButtonFillController } from './components/ButtonFillController';
 import { HomeBuildStackMount } from './components/HomeBuildStackMount';
