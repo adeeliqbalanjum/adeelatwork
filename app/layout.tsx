65f7ec8b1804d964d5f55277ca92d40499e5921b
adeeliqbalanjum <bitm-f20-090@superior.edu.pk>
2026-06-20 16:55:00 +0500

    Revert extra scroll indicator removal

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,6 +43,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body>
         <ButtonFillController />
         <ScrollProgressBar type='bar' color='#070707' strokeSize={2} showPercentage={false} />
+        <ScrollProgressBar position='bottom-right' color='#070707' strokeSize={3} showPercentage />
         {children}
         <HomeHeroOptionBMount />
         <HomeStatsOptionEMount />
