3bdf34766645782c15f39f6ddcee855c62879416
adeeliqbalanjum <bitm-f20-090@superior.edu.pk>
2026-09-05 13:34:36 +0500

    Delete app directory

--- app/process-options/layout.tsx
+++
@@ -1,8 +0,0 @@
-import "../process-extra-options.css";
-import "../process-gallery-fix.css";
-import "../process-real-options.css";
-import "../process-orbit-polish.css";
-
-export default function ProcessOptionsLayout({ children }: { children: React.ReactNode }) {
-  return children;
-}
